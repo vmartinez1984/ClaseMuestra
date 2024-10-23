7cedcbbb49527eceb04e2c6008c9938a8250e487
Víctor Martinez <ahal_tocob@hotmail.com>
2024-10-23 14:22:25 -0600

    Version RC

---
+++ assets/js/site.js
@@ -0,0 +1,9 @@
+function cargarElFooter() {
+    fetch("/pages/templates/footer.html")
+        .then((response) => response.text())
+        .then((data) => document.getElementById('footer').innerHTML = data)
+}
+
+function cargarPlantillas(){
+    cargarElFooter()
+}
